5198c0c511f9f01bed99babb54e03769eb2b75a4
Angeles Joana Osorio Zaragoza <65461669+AngieMoonTS25@users.noreply.github.com>
2024-06-28 23:54:11 -0600

    Add files via upload
    modificaciones para que se vieran los demas proyectos

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 /*selecciones para la barra de navegacion burger */
-const burguer = document.querySelector("#burguer-menu");
+const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
-burguer.addEventListener("click", ()=>{
+burger.addEventListener("click", ()=>{
     ul.classList.toggle("show");
 });
 
 //cerrar el menu burguer
 const navLink= document.querySelectorAll(".nav-link"); 
 
-navLink.forEach((link) => 
-    link.addEventListener("click", ()=> {
+navLink.forEach((link) => {
+    link.addEventListener("click", () => {
         ul.classList.remove("show");
     })
-);
+})
 
 //seleccion de desplazar hacia arriba 
 const scrollUp = document.querySelector("#scroll-up");
